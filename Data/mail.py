cfd01ec1819547c3203b94a1039dc1814fdbb5a5
Audrey <audrey.cornaire@gmail.com>
2020-08-07 16:13:22 +0200

    add - Premier jet de mail_to_csv (non fonctionnel)

---
+++ Data/mail.py
@@ -0,0 +1,6 @@
+class Mail :
+
+    def __init__(self, header, content, path):
+        self.header = header
+        self.content = content
+        self.path = path
--- Data/mail_to_csv.py
+++ Data/mail_to_csv.py
@@ -1,18 +1,49 @@
-import mailbox
+from email.message import Message
+#from pandas.DataFrame import to_csv
+import pandas as pd
+import os
+import email
 import csv
+from mail import Mail
 
-def get_message(message):
-    if not message.is_multipart():
-        return message.get_payload()
-    contents = ""
-    for msg in message.get_payload():
-        contents = contents + str(msg.get_payload()) + '\n'
-    return contents
+PATH = os.path.dirname(os.getcwd())+os.sep
 
-if __name__ == "__main__":
 
-    with open("sampleCsv.csv", "w") as writer:
-    #writer = csv.writer(open("clean_mail.csv", "wb"))
-        for message in mailbox.mbox("archive.mbox"):
-            contents = get_message(message)
-            writer.writerow([message["subject"], message["from"], message["date"],contents])
+def get_files(path):
+    for path, dirs, files in os.walk(maildir):
+        for filename in  files :
+            yield os.path.join(path, filename)
+
+
+def maildir_to_message(maildir):
+    lstMsg=[]
+    for f in get_files(maildir):
+        with open (f, 'r'):
+            message = email.message_from_string(f)
+
+            if message.is_multipart():
+                for part in message.walk():
+                    msgType = part.get_content_type()
+                    msgDispo = str(part.get('Content-Disposition'))
+
+                    # skip any text/plain (txt) attachments
+                    if msgType == 'text/plain' and 'attachment' not in msgDispo:
+                        content = part.get_payload(decode=True)
+                        break
+            else:
+                content = message.get_payload(decode=True)
+            header=message.items()
+            path= os.path.abspath(__file__)#Wrong path !
+            un_msg=Mail(header, content, path)
+            lstMsg.append(un_msg)
+
+    return lstMsg
+
+def to_csv(lstMsg):
+    with open ('sampleCsv.csv', 'w'):
+        df = pd.DataFrame()
+        df.to_csv(index=False)
+
+
+maildir=(PATH+'maildir'+os.sep+'bass-e'+os.sep+'funny'+os.sep)
+print(maildir_to_message(maildir)[2].header)
